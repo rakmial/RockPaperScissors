15f13b3c7b934d774a22d5dee03b5b21f419e135
Joshua Lovelace <rakmial@gmail.com>
2020-08-28 16:12:10 -0700

    generalize test 2, green

--- spec/RockPaperScissors_spec.rb
+++ spec/RockPaperScissors_spec.rb
@@ -5,7 +5,12 @@ describe(RockPaperScissors) do
   it('returns "Tie!" when rock vs. rock') do
     expect(RockPaperScissors.new.shoot("rock","rock")).to(eq("Tie!"))
   end
-  it('returns "Paper beats Rock!" when rock vs paper') do
-    expect(RockPaperScissors.new.shoot("rock","paper")).to(eq("Paper beats Rock!"))
+  it('correctly returns "x beats y" for any combination of rock, paper, or scissors') do
+    expect(RockPaperScissors.new.shoot("rock","paper")).to(eq("paper beats rock!"))
+    expect(RockPaperScissors.new.shoot("paper","rock")).to(eq("paper beats rock!"))
+    expect(RockPaperScissors.new.shoot("paper","scissors")).to(eq("scissors beats paper!"))
+    expect(RockPaperScissors.new.shoot("scissors","paper")).to(eq("scissors beats paper!"))
+    expect(RockPaperScissors.new.shoot("rock","scissors")).to(eq("rock beats scissors!"))
+    expect(RockPaperScissors.new.shoot("scissors","rock")).to(eq("rock beats scissors!"))
   end
 end
