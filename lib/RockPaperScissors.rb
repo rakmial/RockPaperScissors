86fb600b43809af77e7f35c621aad891f4705b41
Joshua Lovelace <rakmial@gmail.com>
2020-08-28 15:56:21 -0700

    test 2 red

--- lib/RockPaperScissors.rb
+++ lib/RockPaperScissors.rb
@@ -8,6 +8,12 @@ class RockPaperScissors
   def shoot(p1_input,p2_input)
     if p1_input == p2_input
       "Tie!"
+    #else
+    #  for i in [0,1,2] do
+    #    if ((p1_input||p2_input) == @win_ray[i]) && \
+    #      ((p1_input||p2_input) == @win_ray[i+1])
+    #      @win_ray[i+1] + " beats " + @win_ray[i] + "!"
+    #  end
     end
   end
 end
--- spec/RockPaperScissors_spec.rb
+++ spec/RockPaperScissors_spec.rb
@@ -5,4 +5,7 @@ describe(RockPaperScissors) do
   it('returns "Tie!" when rock vs. rock') do
     expect(RockPaperScissors.new.shoot("rock","rock")).to(eq("Tie!"))
   end
+  it('returns "Paper beats Rock!" when rock vs paper') do
+    expect(RockPaperScissors.new.shoot("rock","paper")).to(eq("Paper beats Rock!"))
+  end
 end
